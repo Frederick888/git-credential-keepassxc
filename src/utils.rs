18b9e84bf9ec0ec397ea32893790bcb589716b68
Frederick Zhang <frederick888@tsundere.moe>
2022-06-11 01:03:31 +1000

    Replace crate with $crate in logging macros
    [1] https://rust-lang.github.io/rust-clippy/master/index.html#crate_in_macro_def

--- src/utils.rs
+++ src/utils.rs
@@ -32,28 +32,28 @@ const KEEPASS_SOCKET_ENVIRONMENT_VARIABLE: &str = "KEEPASSXC_BROWSER_SOCKET_PATH
 #[macro_export]
 macro_rules! error {
     ($($args:tt)+) => {
-        #[cfg(not(test))] slog::log!(crate::LOGGER.get().unwrap(), slog::Level::Error, "", $($args)+);
+        #[cfg(not(test))] slog::log!($crate::LOGGER.get().unwrap(), slog::Level::Error, "", $($args)+);
         #[cfg(test)] eprintln!("{}: {}", slog::Level::Error, format!($($args)+));
     };
 }
 #[macro_export]
 macro_rules! warn {
     ($($args:tt)+) => {
-        #[cfg(not(test))] slog::log!(crate::LOGGER.get().unwrap(), slog::Level::Warning, "", $($args)+);
+        #[cfg(not(test))] slog::log!($crate::LOGGER.get().unwrap(), slog::Level::Warning, "", $($args)+);
         #[cfg(test)] eprintln!("{}: {}", slog::Level::Warning, format!($($args)+));
     };
 }
 #[macro_export]
 macro_rules! info {
     ($($args:tt)+) => {
-        #[cfg(not(test))] slog::log!(crate::LOGGER.get().unwrap(), slog::Level::Info, "", $($args)+);
+        #[cfg(not(test))] slog::log!($crate::LOGGER.get().unwrap(), slog::Level::Info, "", $($args)+);
         #[cfg(test)] eprintln!("{}: {}", slog::Level::Info, format!($($args)+));
     };
 }
 #[macro_export]
 macro_rules! debug {
     ($($args:tt)+) => {
-        #[cfg(not(test))] slog::log!(crate::LOGGER.get().unwrap(), slog::Level::Debug, "", $($args)+);
+        #[cfg(not(test))] slog::log!($crate::LOGGER.get().unwrap(), slog::Level::Debug, "", $($args)+);
         #[cfg(test)] eprintln!("{}: {}", slog::Level::Debug, format!($($args)+));
     };
 }
